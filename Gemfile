2913b4cdef1d3f778458ff22f4b0fa3fa2e095da
Joel Tanner Jones <jonesjoe@ecu.edu>
2017-02-27 16:20:50 -0500

    Still trying to fix jekyll loading of plugins

--- Gemfile
+++ Gemfile
@@ -1,2 +1,9 @@
 source "https://rubygems.org"
+
+group :jekyll_plugins do
+  gem "bourbon"
+  gem "susy"
+  gem "breakpoint"
+end
+
 gemspec
--- _plugins/sass_plugins.rb
+++
@@ -1,7 +0,0 @@
-# We need to require these here because we can't require them in _config.yml
-# because the jekyll site needs that file and would overwrite that directive
-# if it were in _config.yml
-
-require 'bourbon'
-require 'susy'
-require 'breakpoint'
